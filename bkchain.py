496f504ce907faf1c786d19a2c888ff310151af7
Nikhilesh Chaudhary <nikhileshchaudhary@Nikhileshs-MacBook-Pro.local>
2019-03-15 20:24:51 -0700

    refactor

--- bkchain.py
+++ bkchain.py
@@ -166,22 +166,25 @@ def get_nodes():
     }
     return jsonify(resp), 200
 
+
 @app.route('/nodes/register', methods=['POST'])
 def register_nodes():
-    values = request.form
-    nodes = values.get('nodes').replace(" ", "").split(',')
+    values = request.get_json()
 
+    nodes = values.get('nodes')
+    print(nodes)   
     if nodes is None:
-        return "Invalid list specified Try Again !!", 400
+        return "Error: Please supply a valid list of nodes", 400
 
     for node in nodes:
         blockchain.register_node(node)
 
     response = {
-        'message': 'New nodes were added',
-        'total_nodes': [node for node in blockchain.nodes],
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
     }
     return jsonify(response), 201
+
 
 @app.route('/chain', methods=['GET'])
 def full_blockchain():
